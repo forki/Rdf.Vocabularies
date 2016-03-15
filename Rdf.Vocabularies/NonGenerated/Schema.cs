b13a79bf0e2cb1728d5722eb002d149897bd4767
Tomasz Pluskiewicz <tpluskiewicz@tpluskiewicz.pgs-soft.com>
2016-03-15 14:01:01 +0100

    manually created classes

---
+++ Rdf.Vocabularies/NonGenerated/Schema.cs
@@ -0,0 +1,10 @@
+﻿namespace Vocab
+{
+    public static partial class Schema
+    {
+        /// <summary>
+        /// The name property
+        /// </summary>
+        public const string name = BaseUri + "name";
+    }
+}
