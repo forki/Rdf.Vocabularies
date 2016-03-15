164d48f56432f1e130e07c87c4e7c2cc5df4f29a
Tomasz Pluskiewicz <tpluskiewicz@tpluskiewicz.pgs-soft.com>
2016-03-15 13:57:09 +0100

    initial build

---
+++ Rdf.Vocabularies/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Resources;
+using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Rdf.Vocabularies")]
+[assembly: AssemblyDescription("Statically typed classes generated from various RDF Vocabularies or OWL Ontologies")]
+[assembly: AssemblyProduct("Rdf.Vocabularies")]
+[assembly: AssemblyCopyright("Copyright © 2016 Tomasz Pluskiewicz")]
+[assembly: AssemblyCompany("Tomasz Pluskiewicz T+Code")]
+[assembly: NeutralResourcesLanguage("en")]
