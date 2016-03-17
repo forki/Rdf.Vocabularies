eb85749948332cadf4ff1a2c0714fe7a7b338c60
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2016-03-17 14:12:20 +0100

    complete xsd datatypes

--- Rdf.Vocabularies/NonGenerated/Xsd.cs
+++ Rdf.Vocabularies/NonGenerated/Xsd.cs
@@ -2,22 +2,129 @@
 namespace Vocab
 {
     /// <summary>XML Schema Vocabulary (http://www.w3.org/2001/XMLSchema#).</summary>
+    /// <remarks>As seen on https://www.w3.org/TR/rdf11-concepts/#xsd-datatypes</remarks>
     public static partial class Xsd
     {
         public const string Prefix = "xsd";
 
         public const string BaseUri = "http://www.w3.org/2001/XMLSchema#";
 
-        public const string gYear = BaseUri + "gYear";
-
-        public const string gMonth = BaseUri + "gMonth";
-
-        public const string date = BaseUri + "date";
-
+        /// <summary> Character strings (but not all Unicode character strings) </summary>       
         public const string @string = BaseUri + "string";
 
+        /// <summary> true, false </summary>
+        public const string boolean = BaseUri + "boolean";
+
+        /// <summary> Arbitrary-precision decimal numbers </summary>
+        public const string @decimal = BaseUri + "decimal";
+
+        /// <summary> Arbitrary-size integer numbers </summary>
         public const string integer = BaseUri + "integer";
 
-        public const string boolean = BaseUri + "boolean";
+        /// <summary> 64-bit floating point numbers incl. ±Inf, ±0, NaN </summary>
+        public const string @double = BaseUri + "double";
+
+        /// <summary> 32-bit floating point numbers incl. ±Inf, ±0, NaN </summary>
+        public const string @float = BaseUri + "float";
+
+        /// <summary> Dates (yyyy-mm-dd) with or without timezone </summary>
+        public const string date = BaseUri + "date";
+
+        /// <summary> Times (hh:mm:ss.sss…) with or without timezone </summary>
+        public const string time = BaseUri + "time";
+
+        /// <summary> Date and time with or without timezone </summary>
+        public const string dateTime = BaseUri + "dateTime";
+
+        /// <summary> Date and time with required timezone </summary>
+        public const string dateTimeStamp = BaseUri + "dateTimeStamp";
+
+        /// <summary> Gregorian calendar year </summary>
+        public const string gYear = BaseUri + "gYear";
+
+        /// <summary> Gregorian calendar month </summary>
+        public const string gMonth = BaseUri + "gMonth";
+
+        /// <summary> Gregorian calendar day of the month </summary>
+        public const string gDay = BaseUri + "gDay";
+
+        /// <summary> Gregorian calendar year and month </summary>
+        public const string gYearMonth = BaseUri + "gYearMonth";
+
+        /// <summary> Gregorian calendar month and day </summary>
+        public const string gMonthDay = BaseUri + "gMonthDay";
+
+        /// <summary> Duration of time </summary>
+        public const string duration = BaseUri + "duration";
+
+        /// <summary> Duration of time (months and years only) </summary>
+        public const string yearMonthDuration = BaseUri + "yearMonthDuration";
+
+        /// <summary> Duration of time (days, hours, minutes, seconds only) </summary>
+        public const string dayTimeDuration = BaseUri + "dayTimeDuration";
+
+        /// <summary> -128…+127 (8 bit) </summary>
+        public const string @byte = BaseUri + "byte";
+
+        /// <summary> -32768…+32767 (16 bit) </summary>
+        public const string @short = BaseUri + "short";
+
+        /// <summary> -2147483648…+2147483647 (32 bit) </summary>
+        public const string @int = BaseUri + "int";
+
+        /// <summary> -9223372036854775808…+9223372036854775807 (64 bit) </summary>
+        public const string @long = BaseUri + "long";
+
+        /// <summary> 0…255 (8 bit) </summary>
+        public const string unsignedByte = BaseUri + "unsignedByte";
+
+        /// <summary> 0…65535 (16 bit) </summary>
+        public const string unsignedShort = BaseUri + "unsignedShort";
+
+        /// <summary> 0…4294967295 (32 bit) </summary>
+        public const string unsignedInt = BaseUri + "unsignedInt";
+
+        /// <summary> 0…18446744073709551615 (64 bit) </summary>
+        public const string unsignedLong = BaseUri + "unsignedLong";
+
+        /// <summary> Integer numbers >0 </summary>
+        public const string positiveInteger = BaseUri + "positiveInteger";
+
+        /// <summary> Integer numbers ≥0 </summary>
+        public const string nonNegativeInteger = BaseUri + "nonNegativeInteger";
+
+        /// <summary> Integer numbers <0 </summary>
+        public const string negativeInteger = BaseUri + "negativeInteger";
+
+        /// <summary> Integer numbers ≤0 </summary>
+        public const string nonPositiveInteger = BaseUri + "nonPositiveInteger";
+
+        /// <summary> Hex-encoded binary data </summary>
+        public const string hexBinary = BaseUri + "hexBinary";
+
+        /// <summary> Base64-encoded binary data </summary>
+        public const string base64Binary = BaseUri + "base64Binary";
+
+        /// <summary> Absolute or relative URIs and IRIs </summary>
+        public const string anyURI = BaseUri + "anyURI";
+
+        /// <summary> Language tags per http://tools.ietf.org/html/bcp47 </summary>
+        public const string language = BaseUri + "language";
+
+        /// <summary> Whitespace-normalized strings </summary>
+        public const string normalizedString = BaseUri + "normalizedString";
+
+        /// <summary> Tokenized strings </summary>
+        public const string token = BaseUri + "token";
+
+        /// <summary> XML NMTOKENs </summary>
+        public const string NMTOKEN = BaseUri + "NMTOKEN";
+
+        /// <summary> XML Names </summary>
+        public const string Name = BaseUri + "Name";
+
+        /// <summary> XML NCNames </summary>
+        public const string NCName = BaseUri + "NCName";
+
     }
 }
